3dac57eb8ddd5242b42f395214a01a9272ad5443
Myles McNamara <myles@hostt.net>
2013-12-23 18:31:13 -0500

    quick and dirty reply notification

--- modules/addons/pushover/hooks.php
+++ modules/addons/pushover/hooks.php
@@ -59,5 +59,48 @@ function pushover_ticket_open($vars) {
 	logModuleCall('pushover','hook_ticket_open', $pushover_post_fields, $pushover_resp);
 
 }
+function pushover_ticket_reply($vars) {
+	$ticketid = $vars['ticketid'];
+	$userid = $vars['userid'];
+	$deptid = $vars['deptid'];
+	$deptname = $vars['deptname'];
+	$subject = $vars['subject'];
+	$message = $vars['message'];
+	$priority = $vars['priority'];
 
-add_hook("TicketOpen",1,"pushover_ticket_open"); 
+	$pushover_userkey = po_get_userkey();
+	if (!$pushover_userkey) return false;
+
+	$po_ticket_url = po_get_admin_ticket_url($ticketid);
+	
+	$pushover_api_url = 'https://api.pushover.net/1/messages.json';
+	$pushover_app_token = 'a7HcPjJeGmAtyG4e6tCYqyXk5wc5Xj';
+	$pushover_title = '[Ticket ID: '.$ticketid.'] New Support Ticket Response';
+	$pushover_url = $po_ticket_url;
+	$pushover_url_title = "Open Admin Area to View Ticket";
+	$pushover_message = $message;
+
+	$pushover_post_fields = array(
+			  	'token' => $pushover_app_token,
+			  	'user' => $pushover_userkey,
+			  	'title' => $pushover_title,
+			  	'message' => $pushover_message,
+			  	'url' => $pushover_url,
+			  	'url_title' => $pushover_url_title,
+			  	'priority' => 1
+			  	);
+
+	$pushover_resp = curlCall($pushover_api_url, $pushover_post_fields, $pushover_options);
+
+	$parsed_resp = json_decode($pushover_resp, TRUE);
+
+	if ($parsed_resp['status'] != 1) {
+		logModuleCall('pushover','pushover_server_error', $pushover_post_fields, $pushover_resp);
+	}
+
+	logModuleCall('pushover','hook_ticket_open', $pushover_post_fields, $pushover_resp);
+
+}
+
+add_hook("TicketOpen",1,"pushover_ticket_open");
+add_hook("TicketUserReply",1,"pushover_ticket_reply");
