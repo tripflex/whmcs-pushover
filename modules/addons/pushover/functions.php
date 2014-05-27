361dd3df96e0fbc74eeb9e86a05bc1353ea26268
Myles McNamara <myles@smyl.es>
2014-05-27 18:25:12 -0400

    remove whmcse get_url

--- modules/addons/pushover/functions.php
+++ modules/addons/pushover/functions.php
@@ -15,47 +15,47 @@
 if (!defined("WHMCS"))
 	die("This file cannot be accessed directly");
 
-define( 'PO_ROOT', dirname( __FILE__ ) );
-
-require_once( PO_ROOT . '/inc/whmcse.php' );
-
-function po_get_ssl_url(){
-	global $CONFIG;
-	return $CONFIG['SystemSSLURL'];
-}
 function po_log($description, $request, $response){
 	logModuleCall('pushover', $description, $request, $response);
 }
+
 function po_debug($debugdata, $debugdata2){
 	logModuleCall('pushover','debug', $debugdata, $debugdata2);
 }
-function po_get_url () {
+
+function po_get_url ( $ssl = true ) {
 	global $CONFIG;
-	return $CONFIG['SystemURL'];
+	$ssl_url = $CONFIG['SystemURL'];
+	$nonssl_url = $CONFIG['SystemSSLURL'];
+	if(!$ssl_url){
+		return $nonssl_url;
+	} else {
+		return $ssl_url;
+	}
+
 }
+
 function po_get_customadminpath() {
 	return $GLOBALS['customadminpath'];
 }
+
 function po_get_admin_url(){
 	$customadminpath = po_get_customadminpath();
 	if (!$customadminpath) $customadminpath = "admin";
-	$sslurl = po_get_ssl_url();
-	if (!$sslurl){
-		$whmcsurl = po_get_url();
-	} else {
-		$whmcsurl = $sslurl;
-	}
+	$whmcsurl = po_get_url();
 
 	return $whmcsurl."/".$customadminpath;
 }
+
 function po_get_admin_ticket_url($ticketid){
 	if(po_get_enable_mobile()){
-		$ticket_url = PO_WHMCSe::get_url() . '/mobile/tickets.php?action=view&id=' . $ticketid;
+		$ticket_url = po_get_url() . '/mobile/tickets.php?action=view&id=' . $ticketid;
 	} else {
 		$ticket_url = po_get_admin_url() . '/supporttickets.php?action=viewticket&id=' . $ticketid;
 	}
 	return $ticketurl;
 }
+
 function po_get_userkey(){
     $sql = mysql_query("SELECT value FROM tbladdonmodules WHERE module='pushover' AND setting='userkey'");
         $result = mysql_fetch_array($sql);
